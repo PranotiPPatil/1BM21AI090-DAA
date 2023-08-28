1b83d154e1be4e926d509dab03cf7b97698f6299
Pranoti Prabhakar Patil <139759650+PranotiPPatil@users.noreply.github.com>
2023-08-28 15:44:28 +0530

    C++ Program for Tower of Hanoi

---
+++ TowerOfHonai.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+    if (n == 1) {
+        cout << "Move disk 1 from " << source << " to " << destination << endl;
+        return;
+    }
+
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main() {
+    int num_disks;
+    cout << "Enter the number of disks: ";
+    cin >> num_disks;
+
+    towerOfHanoi(num_disks, 'A', 'B', 'C');
+
+    return 0;
+}
+
